e85a2656b63ac3870d974948dd3eacdfd9edaabe
Tipochec <www.marat12345k@mail.ru>
2025-10-07 12:00:06 +1000

    Create ContactView.vue

---
+++ vue/src/views/ContactView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="contact">
+        <h1>Это страница контактов</h1>
+        <p> Здесь весь контент жесть просто</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactView'
+}
+</script>
+
+
+<style scoped>
+
+.contact {
+    padding: 20px;
+}
+
+</style>
